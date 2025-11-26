d1bdc0f16d99914e10f633af6da4d696d8c0b5ac
contactoia40-byte <contacto.ia4.0@gmail.com>
2025-11-26 18:46:29 +0100

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,52 +1,40 @@
-// Nombre del caché
 const CACHE_NAME = "rebecop-cache-v1";
 
-// Archivos que queremos cachear
 const URLS_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "/manifest.json",
-    "/icon-192.png",
-    "/icon-512.png"
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
-// Instalación del Service Worker
-self.addEventListener("install", event => {
-    console.log("Service Worker instalado");
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(URLS_TO_CACHE);
-            })
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
 });
 
-// Activación del Service Worker
-self.addEventListener("activate", event => {
-    console.log("Service Worker activado");
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.map(key => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            )
-        )
-    );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
 });
 
-// Intercepción de peticiones (fetch)
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
